1464a9e360691178ee97a444ebb3cd8674377376
nitish sir <nitishsir@gmail.com>
2021-06-29 00:25:16 +0530

    exporting

---
+++ node-crash-course-brad-traversy/index.js
@@ -0,0 +1,11 @@
+// console.log('hello world from node js');
+
+// importing object from external module
+const Car = require('./person');//importing class from another file.
+// const myPerson = require('./person');
+
+// console.log(myPerson.name);
+
+//importing class from another file.
+const myCar = new Car('BMW','sports'); //class instantiating
+myCar.tagline();
