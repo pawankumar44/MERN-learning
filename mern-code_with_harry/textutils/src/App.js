051930fbcca251fa42ad41eee7851c7fa8d8a507
nitish sir <nitishsir@gmail.com>
2022-03-20 01:52:00 +0530

    state

--- mern-code_with_harry/textutils/src/App.js
+++ mern-code_with_harry/textutils/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navebar from './components/Navebar';
+import TextForm from './components/TextForm';
+
 
 function App() {
   return (
+    <>
     <Navebar title= "Textutils" aboutText = "About Text"></Navebar>
+    <div className="container my-3"> 
+      <TextForm heading = "Enter The Text To Analyze "></TextForm>
+    </div>
+    </>
   );
 }
 
---
+++ mern-code_with_harry/textutils/src/components/TextForm.js
@@ -0,0 +1,34 @@
+import React ,{useState} from 'react';
+
+export default function TextForm(props) {
+    const [text,setText] = useState('Enter Text Here ');
+
+    const handleUpClick = () =>{
+        console.log("Upper case was clicked "+text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    //event we get from onChange method
+    const handleOnChange = (event) =>{
+        console.log("On change");
+        setText(event.target.value);//need to do because we are handling text from many where.
+        //so it is neccesary to update on onChange in text field also. 
+    }
+
+  return (
+    <>
+    <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-2">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="7"></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Upper Case</button>
+    </div>
+    <div className="container">
+        <h3>Your Text Summary</h3>
+        <p>{text.split(" ").length} Words and {text.length} characters</p>
+    </div>
+    </>
+  )
+}
