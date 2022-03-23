7e4735e7108afa74f7540b954dbdea68357e96dd
nitish sir <nitishsir@gmail.com>
2022-03-23 21:27:04 +0530

    Updates

---
+++ precache-manifest.d79d79c95bbcb857bb745fa905e9709f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6f224a93be5a63ee99a",
+    "url": "/textutils/static/css/main.e633ff66.chunk.css"
+  },
+  {
+    "revision": "c6f224a93be5a63ee99a",
+    "url": "/textutils/static/js/main.c6f224a9.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/textutils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "04b1b421ad4be0398ccb",
+    "url": "/textutils/static/js/2.04b1b421.chunk.js"
+  },
+  {
+    "revision": "249b581e5cf121412222",
+    "url": "/textutils/static/js/runtime~main.249b581e.js"
+  },
+  {
+    "revision": "c4ab9bd51412454225a846750935fff6",
+    "url": "/textutils/index.html"
+  }
+];
