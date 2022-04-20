61c6864781042e3c60491ec0cc5089ad0a7b8a88
Pawan Kumar <pawankatty242@gmail.com>
2022-04-20 21:13:46 +0530

    moongose and routes

--- mern-code_with_harry/inotebook/backend/index.js
+++ mern-code_with_harry/inotebook/backend/index.js
@@ -6,15 +6,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+//available routes 
+//we link routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 //we can write all the routes in same place but this is not the good habit of coding and can access it by appending it with path
 
-// app.get('/v1/login', (req, res) => {
-//     res.send('Hello login!')
-//   })
+app.get('/v1/login', (req, res) => {
+    res.send('Hello login!')
+  })
 
 // app.get('/v2/signup', (req, res) => {
 //     res.send('Hello signup!')
