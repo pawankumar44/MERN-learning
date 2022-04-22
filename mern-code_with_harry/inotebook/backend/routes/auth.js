fe2093b97771bfb4833650ed3f635620e17202ce
Pawan Kumar <pawankatty242@gmail.com>
2022-04-22 23:17:42 +0530

    jwt authentication

--- mern-code_with_harry/inotebook/backend/routes/auth.js
+++ mern-code_with_harry/inotebook/backend/routes/auth.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require("../mongoose_models/User")
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+//jwt authentication
+const JWT_SECRET = "Pawanisalazy$guy";//use this to sign jwt
+
 
 //create a User using : POST by endpoint "/api/auth". Doesn't require auth . No login required
 router.post('/createuser',[
@@ -20,10 +27,15 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({error : "Sorry a user with this email already exists"})
     }
+
+    //salting the password
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password,salt) ;
+    //create a new user 
     user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       })
     //   .then(user => res.json(user))
     //   .catch(err=> {console.log(err)
@@ -34,7 +46,17 @@ router.post('/createuser',[
     //     a: 'thios',
     //     number : 34
     // }
-    res.json({"nice":"nice"})
+
+    //do jwt
+    const data = {
+        user : {
+            id : user.id
+        }
+    }
+    const authToken = jwt.sign(data,JWT_SECRET);
+    // console.log(jwtData);
+    // res.json({"nice":"nice"})
+    res.json({authToken})
     } catch (error) {
         console.error(error.message);
         req.status(500).send("some error occured")
