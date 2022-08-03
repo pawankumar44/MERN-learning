60bb0d654e3adc3690687a181b069b1f28bbd474
Pawan Kumar <pawankatty242@gmail.com>
2022-08-04 03:28:25 +0530

    login backend frontend

--- roadside_coder/mern_chat/frontend/src/components/authentication/Login.js
+++ roadside_coder/mern_chat/frontend/src/components/authentication/Login.js
@@ -1,19 +1,72 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useToast } from '@chakra-ui/react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 const Login = () => {
     //we use state here instead of variable to store value from form input
     const [show, setshow] = useState(false)//() in this we insert the default value
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
+    const [loading,setLoading] = useState(false)
+    const toast = useToast()
+    const navigate = useNavigate()//for navigating after registration or login
 
-    const submitHandler = () => { }
+
+    const submitHandler = async () => {
+        if(!email || !password){
+            toast({
+                title: 'Please Fill all fields',
+                description: "",
+                status: 'warning',
+                duration: 5000,
+                isClosable: true,
+                position:"bottom"
+              })
+              return;
+        }
+        try {
+            setLoading(true)
+            const config = {
+                headers : {
+                    "Content-type" : "application/json"
+                },
+            }
+            const {data} = await axios.post(
+                "/api/user/login",{email,password},
+                config
+            )
+            setLoading(false)
+            toast({
+                title: 'Login successful',
+                description: "",
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+                position:"bottom"
+              })
+              localStorage.setItem("userInfo",JSON.stringify(data))
+              navigate('/chats')
+        } catch (error) {
+            setLoading(false)
+            toast({
+                title: 'Error Occured',
+                description: error.response.data.message,
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+                position:"bottom"
+              })
+        }
+     }
 
     return (
         <VStack spacing="5px">
             <FormControl id='email' isRequired>
                 <FormLabel>Email</FormLabel>
                 <Input placeholder='Enter Your Email'
+                value={email}
                     onChange={(e) => setEmail(e.target.value)}></Input>
             </FormControl>
 
@@ -22,6 +75,7 @@ const Login = () => {
                 <InputGroup>
                     <Input type={show ? "text" : "password"}
                         placeholder='Enter Password'
+                        value={password}
                         onChange={(e) => setPassword(e.target.value)}></Input>
                     <InputRightElement width="4.5rem">
                         <Button height="1.75rem" size="sm"
@@ -40,7 +94,7 @@ const Login = () => {
                  onClick={()=>{
                     setEmail('guest@example.com')
                     setPassword('123456')
-                 }}>
+                 }} isLoading = {loading}>
                     Get Gusest User Credentials</Button>
 
         </VStack>
