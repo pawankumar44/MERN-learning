944fc1d66c18eeee52fb8a2955929a7f04a77c09
Pawan Kumar <pawankatty242@gmail.com>
2022-08-06 15:00:53 +0530

    create a group api

--- roadside_coder/mern_chat/backend/routes/chatRoutes.js
+++ roadside_coder/mern_chat/backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {accessChat,fetchChats} = require('../controllers/chatControllers')
+const {accessChat,fetchChats,createdGroupChat} = require('../controllers/chatControllers')
 const { protect } = require('../middlewares/authMiddleware')
 
 const router = express.Router()
@@ -7,7 +7,7 @@ const router = express.Router()
 //first go through protect middleware, so if user is not logged in then can't access the route
 router.route('/').post(protect,accessChat)//for accessing or creating the chat
 router.route('/').get(protect,fetchChats)//get all of chat from database of particular user 
-// router.route('/group').post(protect,createdGroupChat)//for creation of a group
+router.route('/group').post(protect,createdGroupChat)//for creation of a group
 // router.route('/rename').put(protect,renameGroup)//rename a particular group
 // router.route('/groupremove').put(protect,removeFromGroup)//remove someone or leave the group
 // router.route('/groupadd').put(protect,addToGroup)//Add someone to group
