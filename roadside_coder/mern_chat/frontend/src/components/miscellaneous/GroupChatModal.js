0413eb93c4cee1272669d4eaab8888cf7dc9beb7
Pawan Kumar <pawankatty242@gmail.com>
2022-08-13 02:17:45 +0530

    userBadge

--- roadside_coder/mern_chat/frontend/src/components/miscellaneous/GroupChatModal.js
+++ roadside_coder/mern_chat/frontend/src/components/miscellaneous/GroupChatModal.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
-import {Button, FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, useToast} from '@chakra-ui/react'
+import {Box, Button, Flex, FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, useToast} from '@chakra-ui/react'
 import { ChatState } from '../../Context/ChatProvider'
+import axios from 'axios'
+import UserListItem from '../user_avatar/UserListItem'
+import {baseUrl} from '../../global_varibale_function/gloabl_varibale'
+import UserBadgeItem from '../user_avatar/UserBadgeItem'
 
 const GroupChatModal = ({children}) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -8,12 +12,59 @@ const GroupChatModal = ({children}) => {
     const [selectedUsers, setSelectedUsers] = useState([])//contained selected users in empty array
     const [search, setSearch] = useState("")
     const [searchResult, setSearchResult] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [ loading, setLoading] = useState(false)
     const toast = useToast()
 
-    const handleSearch = () => {
-        
+    const handleSearch = async (query) => {
+        setSearch(query)
+        if(!query){
+          return
+        }
+        try {
+          setLoading(true)
+          const config = {
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${user.token}`,
+            }
+          }
+          const {data} =  await axios.get(`${baseUrl}/api/user?search=${search}`,config)
+          setSearchResult(data)
+          setLoading(false)
+        } catch (error) {
+          toast({
+            title: 'Error Occured!',
+            description: "Failed to Load the search results",
+            status: 'error',
+            duration: 5000,
+            isClosable: true,
+            position: "bottom-left"
+          })
+          setLoading(false)
+        }
     }
+
+    //add to selected users
+    const handleGroup = async (userToAdd) => {
+      //if selected users already have this
+      if(selectedUsers.includes(userToAdd)){
+        toast({
+          title: 'User already added',
+          status: 'warning',
+          duration: 5000,
+          isClosable: true,
+          position: "top"
+        })
+        return
+      }
+      //otherwise add it to the array
+      //spread all of the selected users already there and then add 
+      setSelectedUsers([...selectedUsers,userToAdd])
+    }
+
+    const handleSubmit = () => {}
+
+    const handleDelete = () => {}
 
     //apend this group chat with other chats
     const {user,chats,setChats} = ChatState()//take out three things
@@ -21,7 +72,6 @@ const GroupChatModal = ({children}) => {
     return (
         <>
           <span onClick={onOpen}>{children}</span>
-    
           <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
@@ -38,14 +88,29 @@ const GroupChatModal = ({children}) => {
                     mb={1}
                     onChange={(e)=> handleSearch(e.target.value)}/>
                 </FormControl>
+                {/* selected users */}
+                <Flex
+                 flexWrap="wrap"
+                >{selectedUsers.map((u)=>(
+                  <UserBadgeItem key={user._id}
+                  user={u} handleFunction={()=>handleDelete(u)} />
+                ))}</Flex>
+                {/* render search user */}
+                {loading ? (<div>loading...</div> ): (
+                  //show only 4 results
+                  searchResult ?.slice(0,4).map((user)=>(
+                    <UserListItem key={user._id}
+                    user={user}
+                    handleFunction={()=>handleGroup(user)}/>
+                  ))
+                )}
               </ModalBody>
     
               <ModalFooter>
-                <Button colorScheme='blue' mr={3} onClick={onClose}>
-                  Close
+                <Button colorScheme='blue' mr={3} onClick={handleSubmit}>
+                  Create Chat
                 </Button>
-                <Button variant='ghost'>Secondary Action</Button>
-              </ModalFooter>
+                 </ModalFooter>
             </ModalContent>
           </Modal>
         </>
