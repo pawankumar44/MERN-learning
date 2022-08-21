b86ee3efe8a6a8228ad4cdbee2b9c952d199b049
Pawan Kumar <pawankatty242@gmail.com>
2022-08-21 22:24:53 +0530

    ui for single chat

--- roadside_coder/mern_chat/frontend/src/components/SingleChat.js
+++ roadside_coder/mern_chat/frontend/src/components/SingleChat.js
@@ -1,12 +1,93 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Flex, FormControl, Input, Spinner, Text, useToast } from '@chakra-ui/react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { getSender,getSenderFull } from '../config/ChatLogics'
 import { ChatState } from '../Context/ChatProvider'
+import { baseUrl } from '../global_varibale_function/gloabl_varibale'
 import ProfileModal from './miscellaneous/ProfileModal'
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
 
 const SingleChat = ({fetchAgain,setFetchAgain}) => {
    const {user,selectedChat,setSelectedChat} = ChatState()
+   //state for all messages
+   const [messages, setMessages] = useState([])
+   const [loading, setLoading] = useState(false)
+   const [newMessage, setNewMessage] = useState()
+   const toast = useToast()
+
+   const fetchMessages = async () => {
+    if(!selectedChat){
+      return
+    }
+    try {
+      const config = {
+        headers: {
+          // "Content-type": "application/json",//since it is get request so it will not require content-type
+          Authorization: `Bearer ${user.token}`,
+        }
+      }
+      setLoading(true)
+      const {data} = await axios.get(`${baseUrl}/api/message/${selectedChat._id}`,
+      config)
+      console.log(messages)
+      setMessages(data)
+      
+      setLoading(false)
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description:"Failed to send the Message",
+        // description:error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: "bottom"
+      })
+    }
+   }
+
+   useEffect(() => {
+    fetchMessages()
+   }, [selectedChat])//whenever selectedChat change fetech again also
+   
+
+   const sendMessage = async(event) => {
+    if(event.key==="Enter" && newMessage)
+    try {
+      setNewMessage("")
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        }
+      }
+      const {data} = await axios.post(`${baseUrl}/api/message`,{
+        content:newMessage,
+        chatId:selectedChat._id
+      },config)
+      console.log(data)
+      //what we are getting need to append it with the array of the messages
+      setMessages([...messages,data])
+    } catch (error) {
+      toast({
+        title: 'Error Occured!',
+        description:"Failed to send the Message",
+        // description:error.response.data.message,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: "bottom"
+      })
+
+    }
+   }
+
+   //pass e as event since it is taking event
+   const typingHandler = (e) =>{
+    setNewMessage(e.target.value)
+    //typing indicator logic
+   }
+
   return (
     <>
     {selectedChat ? (
@@ -31,7 +112,25 @@ const SingleChat = ({fetchAgain,setFetchAgain}) => {
         <Box
         justifyContent="flex-end" d="flex" flexDir="column"
         p={3} bg="#E8E8E8" w="100%" h="100%" borderRadius="lg"
-        overflowY="hidden"></Box>
+        overflowY="hidden">
+          {/* messages here */}
+          {loading ? (
+            <Spinner/>
+          ):(
+            <div>
+              {/* messages */}
+            </div>
+          )}
+          {/* input tag for chat input */}
+          <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+            <Input  
+            varient= "filled"
+            bg="#E0E0E0"
+            placeholder='Enter a message..'
+            onChange={typingHandler}
+            value={newMessage}/>
+          </FormControl>
+        </Box>
         </>
         
     ) : (
